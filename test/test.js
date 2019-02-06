d56cda4f0934aac96c98bd382df8cde7e4ad86fd
AhmedAl-Almi <ahmedalmi95@gmail.com>
2019-02-06 16:39:15 +0200

    add style

--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ test('Home route returns a status code of 200', (t) => {
       });
 });
 
-test('test search', (t) => {
+test('test search girls like you', (t) => {
   supertest(router)
       .post("/search")
       .send('girls like you')
@@ -22,7 +22,9 @@ test('test search', (t) => {
       .expect('Content-Type',/json/)
       .end((err, res) => {
           t.error(err);
-          t.equal(res.body,'Spent 24 hours I need more hours with you You spent the weekend Getting even, ooh ooh We spent the late nights Making things right, between us But now it\'s all good baby Roll that Backwood baby And play me close  \'Cause girls like you Run around with guys like me \'Til sundown, when I come through I need a girl like you, yeah yeah  Girls like you Love fun, yeah me too What I want when I come through I need a girl like you, yeah yeah  Yeah yeah yeah Yeah yeah yeah I need a girl like you, yeah yeah  Yeah yeah yeah Yeah yeah yeah I need a girl like you, yeah yeah  I spent last night On the last flight to you ...  ******* This Lyrics is NOT for Commercial use ******* (1409618207658)' ,'test is done');
+          t.equal(res.body,'Spent 24 hours I need more hours with you You spent the weekend Getting even, ooh ooh We spent the late nights Making things right, between us But now it\'s all good baby Roll that Backwood baby And play me close  \'Cause girls like you Run around with guys like me \'Til sundown, when I come through I need a girl like you, yeah yeah  Girls like you Love fun, yeah me too What I want when I come through I need a girl like you, yeah yeah  Yeah yeah yeah Yeah yeah yeah I need a girl like you, yeah yeah  Yeah yeah yeah Yeah yeah yeah I need a girl like you, yeah yeah  I spent last night On the last flight to you ...  ******* This Lyrics is NOT for Commercial use ******* (1409618207658)' ,'test is done girls like you');
           t.end();
       });
 });
+
+
