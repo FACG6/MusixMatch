3d2dc7e87e423bf513b9f75e3d55b9af90b86c14
AhmedAl-Almi <ahmedalmi95@gmail.com>
2019-02-07 10:32:35 +0200

    add readme

--- src/handler.js
+++ src/handler.js
@@ -148,6 +148,5 @@ const handleError = (request, response) => {
     response.writeHead(404, { 'content-type': 'text/html' });
     response.end('<h1>Server Error</h1>')
 }
-if(module){
+
 module.exports = { handleHomePage, handleSongTitle, handleKeywords , handleStatics, handleError }
-}
